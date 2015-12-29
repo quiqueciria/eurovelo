3fb8dea4b0dcb102169ed249fd0329035ef6bef2
quiqueciria <correojerky@gmail.com>
2015-12-29 17:20:00 +0100

    Update About, Google Analytic and buttons socials

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -10,6 +10,17 @@
   <?php echo css('http://fonts.googleapis.com/css?family=Work+Sans:500,600,400') ?>
   <?php echo css('https://fonts.googleapis.com/css?family=Roboto+Slab') ?>
   <?php echo css('assets/css/site.css') ?>
+  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48545782-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
     
 
   <?php if($page->description() != ''): ?>
