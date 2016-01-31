ccaca2132f90acbbe623f79e7439bea1f3318be0
quiqueciria <correojerky@gmail.com>
2016-01-31 18:27:03 +0100

    btn twitter
    Organized the Twitter button together to form of search and fixed up
    the view in mobile.

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -3,10 +3,10 @@
     <?php foreach($pages->visible() as $item): ?>
     <li><a<?php ecco($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title() ?></a></li>
     <?php endforeach ?>
-    <li class="btn_twitter"><a href="https://twitter.com/euroveloes" target="_blank">twitter</a></li>
   </ul>
   <form class="search" role="search" action="<?php echo url('search') ?>">
     <label class="vh" for="search">Search</label>
     <input type="search" class="searchword" name="q" id="search" placeholder="Buscar..."/>
   </form>
+    <div class="btn_twitter"><a href="https://twitter.com/euroveloes" target="_blank">twitter</a></div>
 </nav>
