457f281774c5319b4eccf882ae97cf4c2cf3338f
quiqueciria <correojerky@gmail.com>
2016-05-08 18:01:02 +0200

    Added a dropdown menu
    Pending css style in dropdown menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,7 +1,18 @@
 <nav class="menu cf" role="navigation">
   <ul class="cf">
     <?php foreach($pages->visible() as $item): ?>
-    <li><a<?php ecco($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title() ?></a></li>
+    <li class="menuitem">
+      <a<?php ecco($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title() ?></a>
+      <?php if($item->hasVisibleChildren()): ?>
+      <ul class="submenu">
+        <?php foreach($item->children()->visible() as $item): ?>
+        <li>
+          <a href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a>
+        </li>
+        <?php endforeach ?>
+      </ul>
+      <?php endif ?>
+    </li>
     <?php endforeach ?>
   </ul>
   <form class="search" role="search" action="<?php echo url('search') ?>">
