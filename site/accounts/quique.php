53b9308650d5149d4b20a60e79cac2a44cc2fe41
quiqueciria <correojerky@gmail.com>
2016-07-03 16:24:20 +0200

    Revert ".gitignore"
    This reverts commit f21a04ef0b3da301b3483f02b7767fd543e35727.

---
+++ site/accounts/quique.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: quique
+email: quiqueciria@gmail.com
+password: >
+  $2a$10$xk762z/FtjuGPIyMDvqHtOgtiB2x97WJ/TGBxhD.tlwjf6yAxVD6a
+language: es_LA
+role: admin
+history:
+  - blog/eurovelo-por-extremadura
+  - blog/el-comienzo
+  - blog/eurovelo-espana-parece-que-se-mueve
+  - contact
